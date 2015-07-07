1e14754e212704142bc13fcc24285eb26fc0b838
Wong Yong Jie <yjwong92@gmail.com>
2015-07-07 23:32:46 +0800

    Added multi-page text extraction.

--- examples/text-extraction.js
+++ examples/text-extraction.js
@@ -1,61 +1,72 @@
 var podofo = require('../index');
 
 var pdf = new podofo.PdfMemDocument();
-pdf.Load('/tmp/yanrong.pdf');
-var tokenizer = new podofo.PdfContentsTokenizer(pdf.GetPage(0));
+pdf.Load('/tmp/business-resume.pdf');
 
-var texts = [];
-var inTextBlock = false;
-var textInCurrentBlock = [];
-var token = true;
-var stack;
+/**
+ * Returns the extracted text for a page.
+ */
+var getTextsForPage = function (page) {
+  var tokenizer = new podofo.PdfContentsTokenizer(pdf.GetPage(page));
 
-while (token) {
-  token = tokenizer.ReadNext();
-  switch (token.type) {
-  case podofo.ePdfContentsType_Keyword:
-    switch (token.keyword) {
-    case 'BT':
-      inTextBlock = true;
-      stack = [];
+  var texts = [];
+  var inTextBlock = false;
+  var textInCurrentBlock = [];
+  var token = true;
+  var stack;
+
+  while (token) {
+    token = tokenizer.ReadNext();
+    switch (token.type) {
+    case podofo.ePdfContentsType_Keyword:
+      switch (token.keyword) {
+      case 'BT':
+        inTextBlock = true;
+        stack = [];
+        break;
+      case 'ET':
+        inTextBlock = false;
+        texts.push(textInCurrentBlock.join(''));
+        textInCurrentBlock = [];
+        break;
+      case 'T*':
+      case 'Tf':
+      case 'Td':
+      case 'TD':
+        texts.push(textInCurrentBlock.join(''));
+        textInCurrentBlock = [];
+        break;
+      case 'Tj':
+      case '\'':
+      case '"':
+        textInCurrentBlock.push(stack.pop().GetString());
+        break;
+      case 'TJ':
+        stack.pop().GetArray().forEach(function (obj) {
+          if (obj.IsString()) {
+            textInCurrentBlock.push(obj.GetString());
+          }
+        });
+        break;
+      }
+
       break;
-    case 'ET':
-      inTextBlock = false;
-      texts.push(textInCurrentBlock.join(''));
-      textInCurrentBlock = [];
-      break;
-    case 'Tf':
-    case 'Td':
-    case 'TD':
-      texts.push(textInCurrentBlock.join(''));
-      textInCurrentBlock = [];
-      break;
-    case 'Tj':
-    case '\'':
-    case '"':
-      textInCurrentBlock.push(stack.pop().GetString());
-      break;
-    case 'TJ':
-      stack.pop().GetArray().forEach(function (obj) {
-        if (obj.IsString()) {
-          textInCurrentBlock.push(obj.GetString());
-        }
-      });
+    case podofo.ePdfContentsType_Variant:
+    case podofo.ePdfContentsType_ImageData:
+      if (inTextBlock) {
+        stack.push(token.variant);
+      }
       break;
     }
-
-    break;
-  case podofo.ePdfContentsType_Variant:
-  case podofo.ePdfContentsType_ImageData:
-    if (inTextBlock) {
-      stack.push(token.variant);
-    }
-    break;
   }
-}
 
-console.log(texts
-    .map(Function.prototype.call, String.prototype.trim)
-    .filter(function (text) { return text; })
-);
+  return texts
+      .map(Function.prototype.call, String.prototype.trim)
+      .filter(function (text) { return text; });
+};
+
+var numPages = pdf.GetPageCount();
+for (var i = 0; i < numPages; i++) {
+  console.log(getTextsForPage(i));
+}
 
